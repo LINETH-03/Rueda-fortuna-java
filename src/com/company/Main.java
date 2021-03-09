78e3601c130667b8e156f115284e7d5947dd4c1b
LINETH-03 <lperezh9@miumg.edu.gt>
2021-03-09 10:14:43 -0600

    cambio de ruleta a adivinar el futuro

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,19 +11,19 @@ public class Main {
         //varianble para el switch
         int option;
         while (!salir) {
-            System.out.println("RUEDA DE LA FORTUNA");
-            System.out.println("1. JUGAR");
-            System.out.println("0. SALIR");
+            System.out.println("\n"+"\n"+"QUIERES SABER QUE DEPARA EL DESTINO?");
+            System.out.println("1. QUIERO SABER MI FUTURO");
+            System.out.println("0. SALIR! LE TEMO AL EXITO :(");
 
             try {
-                System.out.println(" Seleccione una opción ");
+                System.out.println(" Ingresa a la opción 1 o ¿Le temes al éxito? ");
                 option = na.nextInt();
                 switch (option) {
                     case 1:
                         aleatorio();
                         break;
                     case 0:
-                        System.out.println("created by: " + "\n" + "LILY");
+                        System.out.println("DESPRECIASTE MIS PODERES ADIVINATORIOS: " + "\n" + " BY: LILY");
                         salir = true;
                         break;
                     default:
@@ -39,15 +39,11 @@ public class Main {
 
 
     public static void aleatorio(){
-        System.out.println("PREMIOS"+"\n"+"DINERO, AMOR, SALUD, PROSPERIDAD, MANGOS");
+       // System.out.println("PREMIOS"+"\n"+"DINERO, AMOR, SALUD, PROSPERIDAD, MANGOS");
 
-        String[] premios ={"DINERO", "AMOR", "SALUD","PROSPERIDAD","MANGOS"};
+        String[] premios ={"VOLVERAS CON TU EX", "TU CRUSH TE HARA CASO", "TENDRAS SALUD Y ESO ES LO IMPORTANTE","ANDARAS ARREBATA'O EN LA JEEPETA","LOGRARAS TODO LO QUE DESEAS"};
         String alee = (premios[new Random().nextInt(premios.length)]);
-        System.out.println("\n"+"TU PREMIO ES:");
-        System.out.println(alee);
-
+        System.out.println("\n"+"PUEDO VER EN TU FUTURO QUE...!  :  "+"\n"+alee);
 
     }
-
-
 }
